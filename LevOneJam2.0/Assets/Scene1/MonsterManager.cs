b784f568be3aec797ba98535a1a597d19e2b4af8
Fehime <fese@itu.dk>
2018-02-09 23:49:27 +0100

    Monster Spawning
    Monster Spawn added to Nicola Scene

---
+++ LevOneJam2.0/Assets/Scene1/MonsterManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+	//public PlayerHealth playerHealth;
+	public GameObject enemy;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints;
+	public GameObject[] enemiesToRemove;
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	/*
+	private void FixedUpdate()
+	{
+	//	if (GameUI.isDead || GameUI.isWin){
+			CancelInvoke();
+			enemiesToRemove = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach (GameObject enemy in enemiesToRemove)
+			{
+				enemy.SetActive(false);
+			}
+	//	}
+	}*/
+
+
+	void Spawn ()
+	{
+		/*if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }*/
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
+
