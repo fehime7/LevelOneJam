d61c60c5cefb3e55a316308a9fa4405dd933ccf2
LuisaZurlo <zurlo.luisa@gmail.com>
2018-02-09 23:20:10 +0100

    Monsters Movement

---
+++ LevOneJam2.0/Assets/Scene1/MonsterMovement3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement3 : MonoBehaviour {
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Strong").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+
+	void Update ()
+	{
+		nav.SetDestination (player.position);
+	}
+}
