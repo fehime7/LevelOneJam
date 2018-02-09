f4503e3d1daa435a08e53bb6acec608c347f6fc7
Nicola Zaltron <nicolazaltra@gmail.com>
2018-02-09 23:18:57 +0100

    new monstertype class

---
+++ LevOneJam2.0/Assets/Scene1/MonsterMovement1_chasingAndEscaping.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement1 : MonoBehaviour {
+		Transform player;
+		UnityEngine.AI.NavMeshAgent nav;
+		bool following;
+
+
+		void Awake ()
+		{
+			player = GameObject.FindGameObjectWithTag ("Player").transform;
+			nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+			(player.getMonsterType() == MonsterType.Monster2) ? following = true : following = false;
+		}
+
+
+		void Update ()
+		{
+			nav.SetDestination (player.position);
+		}
+	}
