bfbd5bf5c053bff8f926466d48f97260e9c8e015
Nicola Zaltron <nicolazaltra@gmail.com>
2018-02-10 14:32:03 +0100

    random monster movement for the ones not chasing neither escaping

--- LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
+++ LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
@@ -8,8 +8,14 @@ public class MonsterMovement : MonoBehaviour {
 
     Transform player;
     UnityEngine.AI.NavMeshAgent nav;
+	float timer = 0.0f;
+	public float randomTimerDelay = 3.0f;
+	public int maxX = 25;
+	public int minX = -25;
+	public int maxY = 25;
+	public int minY = -25;
 
-    void Awake()
+	void Awake()
     {
         if (GameObject.FindGameObjectWithTag("Player"))
         {
@@ -20,20 +26,28 @@ public class MonsterMovement : MonoBehaviour {
 
     void Update()
     {
-        MonsterType playerType = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType;
+		MonsterType playerType = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType;
 
         switch (mType)
         {
             case MonsterType.Monster1:
-                if (playerType == MonsterType.Monster3)
-                {
-                    nav.SetDestination(player.position);
-                }
-                else if (playerType == MonsterType.Monster2)
-                {
-                    Vector3 enemyDirection = 2* gameObject.transform.position - player.position;
-                    nav.SetDestination(enemyDirection);
-                }
+				if (playerType == MonsterType.Monster3)
+				{
+					nav.SetDestination(player.position);
+				}
+				else if (playerType == MonsterType.Monster2)
+				{
+					Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
+					nav.SetDestination(enemyDirection);
+				}
+				//if nor chasing neither escaping
+				else {
+					if (timer > randomTimerDelay)
+					{
+						nav.SetDestination(getRandomPosition());
+						timer = 0;
+					}
+				}
                 break;
             case MonsterType.Monster2:
                 if (playerType == MonsterType.Monster1)
@@ -45,7 +59,16 @@ public class MonsterMovement : MonoBehaviour {
                     Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
                     nav.SetDestination(enemyDirection);
                 }
-                break;
+				//if nor chasing neither escaping
+				else
+				{
+					if (timer > randomTimerDelay)
+					{
+						nav.SetDestination(getRandomPosition());
+						timer = 0;
+					}
+				}
+				break;
             case MonsterType.Monster3:
                 if (playerType == MonsterType.Monster2)
                 {
@@ -56,7 +79,31 @@ public class MonsterMovement : MonoBehaviour {
                     Vector3 enemyDirection = 2 * gameObject.transform.position-player.position;
                     nav.SetDestination(enemyDirection);
                 }
-                break;
+				//if nor chasing neither escaping
+				else
+				{
+					if (timer > randomTimerDelay)
+					{
+						nav.SetDestination(getRandomPosition());
+						timer = 0;
+					}
+				}
+				break;
         }
-    }
+		timer += Time.deltaTime;
+	}
+
+	private Vector3 getRandomPosition() {
+		float x;
+		float y;
+		float z;
+		Vector3 pos;
+
+		x = Random.Range(minX, maxX);
+		y = 5;
+		z = Random.Range(minY, maxY);
+		pos = new Vector3(x, y, z);
+
+		return pos;
+	}
 }
