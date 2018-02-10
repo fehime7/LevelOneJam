3bb0e98eb85a8f29c080e69dd9c46936ad11d181
Julian Anthony Abela <juab@itu.dk>
2018-02-10 15:12:14 +0100

    Added Reactions

--- LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
+++ LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
@@ -6,8 +6,10 @@ public class MonsterMovement : MonoBehaviour {
 
     public MonsterType mType;
 
+    bool playerDetected = false;
     Transform player;
     UnityEngine.AI.NavMeshAgent nav;
+    private GameObject[] reactions;
 
     void Awake()
     {
@@ -18,49 +20,107 @@ public class MonsterMovement : MonoBehaviour {
         }
     }
 
+    void Start()
+    {
+        reactions = new GameObject[2];
+
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            Transform reactionsTrans = gameObject.transform.GetChild(i).transform;
+
+            if (reactionsTrans.name == "Reactions")
+            {
+                for (int j = 0; j < reactionsTrans.childCount; j++)
+                {
+                    GameObject modelObj = reactionsTrans.GetChild(j).gameObject;
+
+                    reactions[j] = modelObj;
+                    modelObj.SetActive(false);
+                }
+            }
+        }
+    }
+
     void Update()
     {
         MonsterType playerType = player.GetComponent<PlayerMovement>().currentType;
         float playerDistance = Vector3.Distance(gameObject.transform.position, player.position);
 
-        if (mType != playerType && playerDistance < 20)
-        {            
-            switch (mType)
+        if (mType != playerType)
+        {
+            if (playerDistance < 20)
             {
-                case MonsterType.Monster1:
-                    if (playerType == MonsterType.Monster3)
-                    {
-                        nav.SetDestination(player.position);
-                    }
-                    else if (playerType == MonsterType.Monster2)
-                    {
-                        Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
-                        nav.SetDestination(enemyDirection);
-                    }
-                    break;
-                case MonsterType.Monster2:
-                    if (playerType == MonsterType.Monster1)
-                    {
-                        nav.SetDestination(player.position);
-                    }
-                    else if (playerType == MonsterType.Monster3)
-                    {
-                        Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
-                        nav.SetDestination(enemyDirection);
-                    }
-                    break;
-                case MonsterType.Monster3:
-                    if (playerType == MonsterType.Monster2)
-                    {
-                        nav.SetDestination(player.position);
-                    }
-                    else if (playerType == MonsterType.Monster1)
-                    {
-                        Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
-                        nav.SetDestination(enemyDirection);
-                    }
-                    break;
+                switch (mType)
+                {
+                    case MonsterType.Monster1:
+                        if (playerType == MonsterType.Monster3)
+                        {
+                            Chase();
+                        }
+                        else if (playerType == MonsterType.Monster2)
+                        {
+                            Avoid();
+                        }
+                        break;
+                    case MonsterType.Monster2:
+                        if (playerType == MonsterType.Monster1)
+                        {
+                            Chase();
+                        }
+                        else if (playerType == MonsterType.Monster3)
+                        {
+                            Avoid();
+                        }
+                        break;
+                    case MonsterType.Monster3:
+                        if (playerType == MonsterType.Monster2)
+                        {
+                            Chase();
+                        }
+                        else if (playerType == MonsterType.Monster1)
+                        {
+                            Avoid();
+                        }
+                        break;
+                }
+            } else
+            {
+                playerDetected = false;
             }
         }
+    }
+
+    void Chase()
+    {
+        nav.SetDestination(player.position);
+        
+        if (!playerDetected)
+        {
+            StartCoroutine(React(true));
+            playerDetected = true;
+        }
+    }
+
+    void Avoid()
+    {
+        Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
+        nav.SetDestination(enemyDirection);
+
+        if (!playerDetected)
+        {
+            StartCoroutine(React(false));
+            playerDetected = true;
+        }
+    }
+
+    IEnumerator React(bool positive)
+    {
+        if (positive) reactions[0].SetActive(true);
+        else reactions[1].SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        if (positive) reactions[0].SetActive(false);
+        else reactions[1].SetActive(false);
     }
 }
