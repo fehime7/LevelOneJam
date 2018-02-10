54b8b4d4c9408a551bf261ba61a74a22e0e69cb6
Julian Anthony Abela <juab@itu.dk>
2018-02-10 17:51:04 +0100

    Changed Pickups to Triggers

--- LevOneJam2.0/Assets/Scripts/PickupController.cs
+++ LevOneJam2.0/Assets/Scripts/PickupController.cs
@@ -6,14 +6,11 @@ public class PickupController : MonoBehaviour {
 
     public MonsterType mType;
 
-    void OnCollisionEnter(Collision col)
+    void OnTriggerEnter(Collider col)
     {
-        Debug.Log(col.gameObject.name);
-
         if (col.gameObject.name == "Player")
         {
-			Debug.Log("puickUp mask");
-			col.gameObject.GetComponent<PlayerMovement>().Mutate(mType);
+            col.gameObject.GetComponent<PlayerMovement>().Mutate(mType);
             Destroy(this.gameObject);
         }
     }
