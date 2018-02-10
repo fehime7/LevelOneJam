903a319fe3cc83e91b9b510434f181d783c0f741
Fehime <fese@itu.dk>
2018-02-10 15:39:33 +0100

    Mask Spawning edited in Julian Scene
    mask spawning edited in the Julian Scene

---
+++ LevOneJam2.0/Assets/Julian/MaskController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class MaskController : MonoBehaviour
+{
+	//public PlayerHealth playerHealth;
+	public GameObject[] mask;
+	private float spawnTime = 5f;
+	private float deSpawnTime = 8f;
+	public Transform[] spawnPoints;
+	public GameObject[] masksToRemove;
+	int maskIndex;
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		InvokeRepeating ("DeSpawn", deSpawnTime, deSpawnTime);
+		maskIndex = Random.Range (0, mask.Length);
+
+	}
+
+	/*
+	private void FixedUpdate()
+	{
+	//	if (GameUI.isDead || GameUI.isWin){
+			CancelInvoke();
+			enemiesToRemove = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach (GameObject enemy in enemiesToRemove)
+			{
+				enemy.SetActive(false);
+			}
+	//	}
+	}*/
+
+
+	void Spawn ()
+	{
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		maskIndex = Random.Range (0, mask.Length);
+
+
+		Instantiate (mask[maskIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+
+	void DeSpawn  () 
+	{
+
+		masksToRemove = GameObject.FindGameObjectsWithTag ("PickUp");
+
+		/*
+		 foreach (GameObject mask[maskInde] in masksToRemove)
+		{
+			//mask.SetActive(false);
+			Destroy (mask[maskIndex]);
+		}
+		*/
+
+		for(int i=0 ; i<masksToRemove.Length; i++){
+
+			Destroy (masksToRemove [i]);
+		}
+	}
+}
+
+
