b1fd7abf069a5456289529be702cb2cd5e0d26a9
Julian Anthony Abela <juab@itu.dk>
2018-02-10 13:45:39 +0100

    Updated Enemy Movements

--- LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
+++ LevOneJam2.0/Assets/Nicola/MonsterMovement.cs
@@ -20,39 +20,41 @@ public class MonsterMovement : MonoBehaviour {
 
     void Update()
     {
-        switch(mType)
+        MonsterType playerType = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType;
+
+        switch (mType)
         {
             case MonsterType.Monster1:
-                if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster3)
+                if (playerType == MonsterType.Monster3)
                 {
                     nav.SetDestination(player.position);
                 }
-                else if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster2)
+                else if (playerType == MonsterType.Monster2)
                 {
-                    Vector3 enemyDirection = gameObject.transform.position - player.position;
-                    nav.SetDestination(enemyDirection); // running away from player
+                    Vector3 enemyDirection = 2* gameObject.transform.position - player.position;
+                    nav.SetDestination(enemyDirection);
                 }
                 break;
             case MonsterType.Monster2:
-                if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster1)
+                if (playerType == MonsterType.Monster1)
                 {
                     nav.SetDestination(player.position);
                 }
-                else if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster3)
+                else if (playerType == MonsterType.Monster3)
                 {
-                    Vector3 enemyDirection = gameObject.transform.position - player.position;
-                    nav.SetDestination(enemyDirection); // running away from player
+                    Vector3 enemyDirection = 2 * gameObject.transform.position - player.position;
+                    nav.SetDestination(enemyDirection);
                 }
                 break;
             case MonsterType.Monster3:
-                if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster2)
+                if (playerType == MonsterType.Monster2)
                 {
                     nav.SetDestination(player.position);
                 }
-                else if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentType == MonsterType.Monster1)
+                else if (playerType == MonsterType.Monster1)
                 {
-                    Vector3 enemyDirection = gameObject.transform.position-player.position;
-                    nav.SetDestination(enemyDirection); // running away from player
+                    Vector3 enemyDirection = 2 * gameObject.transform.position-player.position;
+                    nav.SetDestination(enemyDirection);
                 }
                 break;
         }
