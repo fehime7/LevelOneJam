ef514a83f7e7a864aa7c959a262c20d0fe564c10
Nicola Zaltron <nicolazaltra@gmail.com>
2018-02-11 13:30:57 +0100

    last stuff

--- LevOneJam2.0/Assets/Scripts/FadeAlpha.cs
+++ LevOneJam2.0/Assets/Scripts/FadeAlpha.cs
@@ -8,11 +8,15 @@ using UnityEngine.SceneManagement;
 
     private bool startedFading = false;
     private MonsterManager monManager;
+	public AudioSource audioSource;
+	public AudioClip killSound;
 
     private void Start() {
         //test  		StartCoroutine(FadeOut3D(this.transform, 0, true, 1));
         monManager = (MonsterManager)FindObjectOfType(typeof(MonsterManager));
-    }
+		audioSource = (AudioSource)FindObjectOfType(typeof(AudioSource));
+
+	}
 
 	void Update()
 	{
@@ -23,6 +27,7 @@ using UnityEngine.SceneManagement;
 
 	public void kill() {
         monManager.enemies.Remove(this.gameObject);
+		
 
 		if (gameObject.active && !startedFading)
 		{
@@ -31,6 +36,7 @@ using UnityEngine.SceneManagement;
 			Instantiate(Resources.Load("EnemyDeath"), gameObject.transform.position, Quaternion.Euler(Vector3.zero), null);
 			//gameObject.AddComponent<AudioSource>().PlayOneShot((AudioClip)Resources.Load("Audio/Sounds/203976__thatbennyguy__female-hurt"), 1.0f);
 		}
+		//audioSource.PlayOneShot(killSound);
 	}
 	public static IEnumerator FadeOut3D(Transform t, float targetAlpha, bool isVanish, float duration)
 	{
